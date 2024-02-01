426b6ad968287d66f69d03d955dfdea302f160c2
Work <Work@MSI>
2024-02-01 10:29:23 -0600

    Where we ended on Feb 1

---
+++ Feb1-Chapter2Stuff-Continued/Feb1-Chapter2Stuff-Continued.cpp
@@ -0,0 +1,38 @@
+
+
+#include <iostream>
+
+using std::cout; 
+using std::endl; 
+
+int main()
+{
+    //std::cout << "Hello World!\n";
+
+    //int bigNumber = 2'147'483'647 + 1; //(2 ^ 31) - 1
+
+    //std::cout.imbue(std::locale(""));
+    //std::cout << bigNumber << std::endl; 
+
+
+    //unsigned int a = pow(2, 32) - 1;
+    //cout << std::hex;
+    //cout << a << endl;
+    //cout << (a >> 8) << endl;
+    //cout << (a >> 16) << endl;
+    //cout << (a >> 24) << endl;
+
+    //cout << &a << endl; 
+
+    //int double = 1; 
+
+    char snake_five = '5';
+
+    //float total / count = 43.2; 
+
+    cout << "25 inches is equal to " << 25 / 12 << " feet (foot) " <<
+        " and this many inches: " << 25 % 12 << endl; 
+
+
+    return 0; 
+}
